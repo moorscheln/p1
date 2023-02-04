f539b8056098a3e532024a263cdac16584d0403b
michaelorscheln <michaelorscheln@crimson.ua.edu>
2023-02-04 16:00:51 -0600

    functions

--- functions.cpp
+++ functions.cpp
@@ -53,18 +53,73 @@ public:
         }
         array[size++] = v;
     }
-    void addFront(elmtype v);
+    void addFront(Elmtype v)
+    {
+        if (size == capacity)
+        {
+            capacity *= 2;
+            Elmtype *new_array = new Elmtype[capacity];
+            for (int i = 0; i < size; i++)
+            {
+                new_array[i + 1] = array[i];
+            }
+            delete[] array;
+            array = new_array;
+        }
+        size++;
+        for (int i = size - 1; i > 0; i--)
+        {
+            array[i] = array[i - 1];
+        }
+        array[0] = v;
+    }
 
     // Delete elements
-    void delEnd();
-    void delFront();
+    void delEnd()
+    {
+        size--;
+        if (size < capacity / 4)
+        {
+            capacity /= 2;
+            Elmtype *new_array = new Elmtype[capacity];
+            for (int i = 0; i < size; i++)
+            {
+                new_array[i] = array[i];
+            }
+            delete[] array;
+            array = new_array;
+        }
+    }
+    void delFront()
+    {
+        if (size == 0)
+            return;
+
+        front = (front + 1) % capacity;
+        size--;
+
+        if (size == 0)
+            front = rear = -1;
+    }
 
     // Getters
-    int length();
-    int capacity();
+    int length()
+    {
+        return length;
+    }
+    int capacity()
+    {
+        return capacity;
+    }
 
     // Utility functions
-    void clear();
+    void clear()
+    {
+        size = 0;
+        capacity = 2;
+        delete[] data;
+        data = new Elmtype[capacity];
+    }
     Elmtype QuickSelect(int k);
     Elmtype WCSelect(int k);
     void stableSort();
@@ -105,161 +160,121 @@ public:
 //     return array[i];
 // }
 
-template <typename Elmtype>
-void CircularDynamicArray<Elmtype>::addEnd(Elmtype v)
-{
-    if (size == capacity)
-    {
-        capacity *= 2;
-        Elmtype *new_array = new Elmtype[capacity];
-        for (int i = 0; i < size; i++)
-        {
-            new_array[i] = array[i];
-        }
-        delete[] array;
-        array = new_array;
-    }
-    array[size++] = v;
-}
+// template <typename Elmtype>
+// void CircularDynamicArray<Elmtype>::addEnd(Elmtype v)
+// {
+//     if (size == capacity)
+//     {
+//         capacity *= 2;
+//         Elmtype *new_array = new Elmtype[capacity];
+//         for (int i = 0; i < size; i++)
+//         {
+//             new_array[i] = array[i];
+//         }
+//         delete[] array;
+//         array = new_array;
+//     }
+//     array[size++] = v;
+// }
 
-template <typename Elmtype>
-void CircularDynamicArray<Elmtype>::addFront(Elmtype v)
-{
-    if (size == capacity)
-    {
-        capacity *= 2;
-        Elmtype *new_array = new Elmtype[capacity];
-        for (int i = 0; i < size; i++)
-        {
-            new_array[i + 1] = array[i];
-        }
-        delete[] array;
-        array = new_array;
-    }
-    size++;
-    for (int i = size - 1; i > 0; i--)
-    {
-        array[i] = array[i - 1];
-    }
-    array[0] = v;
-}
+// delFront()
+// {
+//     size--;
+//     for (int i = 0; i < size; i++)
+//     {
+//         array[i] = array[i + 1];
+//     }
+//     if (size < capacity / 4)
+//     {
+//         capacity /= 2;
+//         Elmtype *new_array = new Elmtype[capacity];
+//         for (int i = 0; i < size; i++)
+//         {
+//             new_array[i] = array[i];
 
-template <typename Elmtype>
-void CircularDynamicArray<Elmtype>::delEnd()
-{
-    size--;
-    if (size < capacity / 4)
-    {
-        capacity /= 2;
-        Elmtype *new_array = new Elmtype[capacity];
-        for (int i = 0; i < size; i++)
-        {
-            new_array[i] = array[i];
-        }
-        delete[] array;
-        array = new_array;
-    }
-}
+//             template <typename Elmtype>
+//             void CircularDynamicArray<Elmtype>::delFront()
+//             {
+//                 // Check if the array is empty
+//                 if (size == 0)
+//                     return;
 
-template <typename Elmtype>
-void CircularDynamicArray<Elmtype>::delFront()
-{
-    size--;
-    for (int i = 0; i < size; i++)
-    {
-        array[i] = array[i + 1];
-    }
-    if (size < capacity / 4)
-    {
-        capacity /= 2;
-        Elmtype *new_array = new Elmtype[capacity];
-        for (int i = 0; i < size; i++)
-        {
-            new_array[i] = array[i];
+//                 // Decrement size by 1
+//                 size--;
 
-            template <typename Elmtype>
-            void CircularDynamicArray<Elmtype>::delFront()
-            {
-                // Check if the array is empty
-                if (size == 0)
-                    return;
+//                 // Shift the remaining elements towards the front
+//                 for (int i = 0; i < size; i++)
+//                 {
+//                     data[i] = data[i + 1];
+//                 }
 
-                // Decrement size by 1
-                size--;
+//                 // Shrink the capacity if only 25% of the array is being used
+//                 if (size < (capacity / 4))
+//                 {
+//                     capacity /= 2;
+//                     Elmtype *temp = new Elmtype[capacity];
+//                     for (int i = 0; i < size; i++)
+//                     {
+//                         temp[i] = data[i];
+//                     }
+//                     delete[] data;
+//                     data = temp;
+//                 }
+//             }
 
-                // Shift the remaining elements towards the front
-                for (int i = 0; i < size; i++)
-                {
-                    data[i] = data[i + 1];
-                }
+// template <typename Elmtype>
+// int CircularDynamicArray<Elmtype>::length()
+// {
+//     return size;
+// }
 
-                // Shrink the capacity if only 25% of the array is being used
-                if (size < (capacity / 4))
-                {
-                    capacity /= 2;
-                    Elmtype *temp = new Elmtype[capacity];
-                    for (int i = 0; i < size; i++)
-                    {
-                        temp[i] = data[i];
-                    }
-                    delete[] data;
-                    data = temp;
-                }
-            }
+// template <typename Elmtype>
+// int CircularDynamicArray<Elmtype>::capacity()
+// {
+//     return capacity;
+// }
 
-            template <typename Elmtype>
-            int CircularDynamicArray<Elmtype>::length()
-            {
-                return size;
-            }
+// template <typename Elmtype>
+// void CircularDynamicArray<Elmtype>::clear()
+// {
+//     size = 0;
+//     capacity = 2;
+//     delete[] data;
+//     data = new Elmtype[capacity];
+// }
 
-            template <typename Elmtype>
-            int CircularDynamicArray<Elmtype>::capacity()
-            {
-                return capacity;
-            }
+// template <typename Elmtype>
+// Elmtype CircularDynamicArray<Elmtype>::QuickSelect(int k)
+// {
+//     // Implement quick select algorithm
+// }
 
-            template <typename Elmtype>
-            void CircularDynamicArray<Elmtype>::clear()
-            {
-                size = 0;
-                capacity = 2;
-                delete[] data;
-                data = new Elmtype[capacity];
-            }
+// template <typename Elmtype>
+// Elmtype CircularDynamicArray<Elmtype>::WCSelect(int k)
+// {
+//     // Implement worst case O(N) time algorithm
+// }
 
-            template <typename Elmtype>
-            Elmtype CircularDynamicArray<Elmtype>::QuickSelect(int k)
-            {
-                // Implement quick select algorithm
-            }
+// template <typename Elmtype>
+// void CircularDynamicArray<Elmtype>::stableSort()
+// {
+//     // Implement comparison based stable sort algorithm
+// }
 
-            template <typename Elmtype>
-            Elmtype CircularDynamicArray<Elmtype>::WCSelect(int k)
-            {
-                // Implement worst case O(N) time algorithm
-            }
+// template <typename Elmtype>
+// int CircularDynamicArray<Elmtype>::linearSearch(Elmtype e)
+// {
+//     // Implement linear search algorithm
+// }
 
-            template <typename Elmtype>
-            void CircularDynamicArray<Elmtype>::stableSort()
-            {
-                // Implement comparison based stable sort algorithm
-            }
+// template <typename Elmtype>
+// int CircularDynamicArray<Elmtype>::binSearch(Elmtype e)
+// {
+//     // Implement binary search algorithm
+// }
 
-            template <typename Elmtype>
-            int CircularDynamicArray<Elmtype>::linearSearch(Elmtype e)
-            {
-                // Implement linear search algorithm
-            }
-
-            template <typename Elmtype>
-            int CircularDynamicArray<Elmtype>::binSearch(Elmtype e)
-            {
-                // Implement binary search algorithm
-            }
-
-            template <typename Elmtype>
-            void CircularDynamicArray<Elmtype>::reverse()
-            {
-                // Reverse the order of the elements in the array
-            }
+// template <typename Elmtype>
+// void CircularDynamicArray<Elmtype>::reverse()
+// {
+//     // Reverse the order of the elements in the array
+// }
